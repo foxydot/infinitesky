c8a294eac186e80b035087f934e8075eb2cf961a
CMO <catherine@msdlab.com>
2018-06-14 14:29:58 -0400

    cole's list homepage

--- system/assets/themes/infinitesky/lib/js/theme-jquery.js
+++ system/assets/themes/infinitesky/lib/js/theme-jquery.js
@@ -22,26 +22,10 @@ jQuery(document).ready(function($) {
 			return '<li class="separator menu-item">|</li>';
 		}
 	});
-	
-	$('.section.expandable .expand').click(function(){
-	    var target = $(this).parents('.section-body').find('.content');
-	    console.log(target);
-	    if(target.hasClass('open')){
-            target.removeClass('open');
-            $(this).html('MORE <i class="fa fa-angle-down"></i>');
-	    } else {
-	        target.addClass('open');
-	        $(this).html('LESS <i class="fa fa-angle-up"></i>');
-	    }
-	});
 
-	$('.gform_wrapper .gform_body li.gfield').each(function(){
-		$(this).children('label').before($(this).children('.ginput_container'));
-	});
-
-    $('.genesis-teaser').matchHeight();
+	$('.genesis-teaser').matchHeight();
     $('.equalize').matchHeight();
 
-    $('.sectioned-page-wrapper .section.align-buttons-bottom .section-body .container .section-content .btn').wrap('<div class="btn-wrapper"></div>');
+    $('.section.align-buttons-bottom .section-content a.btn').wrap('<div class="btn-wrapper"></div>');
 
 });
