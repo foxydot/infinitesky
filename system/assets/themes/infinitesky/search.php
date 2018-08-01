e4fada253bab5e2be6897879675eb539ccec68c9
CMO <catherine@msdlab.com>
2018-08-01 15:55:28 -0400

    Hupspot tweaks

--- system/assets/themes/infinitesky/search.php
+++ system/assets/themes/infinitesky/search.php
@@ -11,6 +11,8 @@ add_action( 'msdlab_title_area', 'msdlab_do_search_title' );
 add_action( 'genesis_entry_header', 'genesis_do_post_title');
 remove_action( 'genesis_entry_header', 'genesis_post_info',12);
 add_action('genesis_entry_content','msdlab_read_search');
+
+remove_all_actions('genesis_after_content_sidebar_wrap');
 /**
  * Echo the title with the search term.
  *
