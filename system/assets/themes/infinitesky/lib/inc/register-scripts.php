b0f089fd917184f0289c43f82fb2c04cb3371cc9
CMO <catherine@msdlab.com>
2018-08-06 13:10:42 -0400

    IE BS fix

--- system/assets/themes/infinitesky/lib/inc/register-scripts.php
+++ system/assets/themes/infinitesky/lib/inc/register-scripts.php
@@ -23,11 +23,8 @@ function msdlab_add_styles() {
             wp_enqueue_style('msd-homepage-style',get_stylesheet_directory_uri().'/lib/css/homepage.css',$queue);
             $queue[] = 'msd-homepage-style';
         }
-        if($is_IE){
-            print '<script>alert("IE sucks");</script>';
-            wp_enqueue_style('ie-style',get_stylesheet_directory_uri().'/lib/css/ie.css',$queue);
-            $queue[] = 'ie-style';
-        }
+        wp_enqueue_style('ie-style',get_stylesheet_directory_uri().'/lib/css/ie.css',$queue);
+        $queue[] = 'ie-style';
     }
 }
 
